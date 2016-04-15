02dd116dbe77241d4c616cefe9610be83afeca96
Marck <mercercountynj@gmail.com>
2016-04-15 14:39:31 -0400

    github api user card

--- app/assets/javascripts/components/message.es6.jsx
+++ app/assets/javascripts/components/message.es6.jsx
@@ -1,13 +1,67 @@
-class Message extends React.Component {
-  render () {
-    return (
-      <div>
-        <div>Hello {this.props.name}!</div>
-      </div>
-    );
-  }
-}
+var Card = React.createClass({
+    getInitialState: function() {
+        return {};
+    },
 
-Message.propTypes = {
-  name: React.PropTypes.string
-};
+    componentDidMount: function() {
+        var component = this;
+        $.get("https://api.github.com/users/" + this.props.login, function(data) {
+            component.setState(data);
+        });
+    },
+    render: function(){
+        return (
+            <div>
+                <img src={this.state.avatar_url} width="80" />
+                <h3>{this.state.name}</h3>
+                <hr/>
+            </div>
+        );
+    }
+});
+
+
+var Form = React.createClass({
+    //*********** otherwise form submits normally and refreshes the page ***********
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var loginInput = ReactDOM.findDOMNode(this.refs.login);
+        // ad the github card here
+        this.props.addCard(loginInput.value);
+        loginInput.value = '';
+    },
+
+    render: function() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="Github Username" ref="login" />
+                <button>Add</button>
+            </form>
+        );
+    }
+});
+
+var Main = React.createClass({
+    getInitialState: function () {
+        return {logins: []};
+    },
+
+    addCard: function(loginToAdd) {
+        this.setState({logins: this.state.logins.concat(loginToAdd)});
+    },
+
+    render: function () {
+        var cards = this.state.logins.map(function(login) {
+            return (<Card login={login} />);
+        });
+        return (
+            <div>
+                <Form addCard={this.addCard} />
+                {cards}
+            </div>
+        )
+    }
+});
+
+
+
